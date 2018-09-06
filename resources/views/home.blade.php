595b64516a4a1c088890ecd9c2c15bb18a713bdb
mufarooq3 <mufarooq3@gmail.com>
2018-09-06 12:06:52 +0500

    Rounding Problem

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -219,11 +219,11 @@
     function success(data1) {
         console.log(data1);
         data=parseFloat(data1);
-        $('#price').text("$"+data);
-        $('#normalPrice').text("$"+(data-(data*0.1))+" - "+"$"+data);
-        $('#cheapPrice').text("$"+(data-(data*0.35))+" - "+"$"+(data-(data*0.1)));
-        $('#superCheapPrice').text("$"+(data-(data*0.5))+" - "+"$"+(data-(data*0.35)));
-        $('#hyperCheapPrice').text("< $"+(data-(data*0.5)))
+        $('#price').text("$"+data.toFixed(2));
+        $('#normalPrice').text("$"+(data-(data*0.1).toFixed(2))+" - "+"$"+data.toFixed(2));
+        $('#cheapPrice').text("$"+(data-(data*0.35).toFixed(2))+" - "+"$"+(data-(data*0.1)).toFixed(2));
+        $('#superCheapPrice').text("$"+(data-(data*0.5).toFixed(2))+" - "+"$"+(data-(data*0.35)).toFixed(2));
+        $('#hyperCheapPrice').text("< $"+(data-(data*0.5)).toFixed(2))
     }
 </script>
 
